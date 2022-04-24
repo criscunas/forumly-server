1f1ae3218c5899d72a50d5d382744d44ccb23450
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-04-24 15:52:18 -0400

    jwt middleware, same as last server

---
+++ middleware/authenticate.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const JWT_S = process.env.JWTS;
+
+const authenticate = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send("Please login");
+  }
+
+  const authToken = req.headers.authorization.split(" ")[1];
+
+  jwt.verify(authToken, JWT_S, (err, decoded) => {
+    if (err) {
+      return res.status(400).send("invalid auth token");
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+module.exports = authenticate;
