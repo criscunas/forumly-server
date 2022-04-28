22d829769b1c6100f6097927553ce42ed7c5f044
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-04-28 00:57:25 -0400

    error

--- server.js
+++ server.js
@@ -27,6 +27,21 @@ app.use("/newsfeed", newsRoute);
 app.use("/categories", categoryRoute);
 
 
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
 module.exports = app;
 
 
